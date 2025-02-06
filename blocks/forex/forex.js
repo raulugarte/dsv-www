8e91f2a9cd62a822ce1fdc838b0d80a33d6a1eac
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-02-06 15:28:02 +0100

    Update forex.js

--- blocks/forex/forex.js
+++ blocks/forex/forex.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
   const container = document.createElement('table');
-  const url = 'https://20092-securbankdemo-stage.adobeio-static.net/api/v1/web/dx-excshell-1/forex?baseCurrency=AUD';
+  const url = 'https://20092-securbankdemo-stage.adobeio-static.net/api/v1/web/dx-excshell-1/forex?baseCurrency=EUR';
   const options = {};
   const forexReq = await fetch(url, options);
   const index = await forexReq.json();
